39fd135a294514b8b667a4de2f3395830b837ac7
luis angel <20luis1angel7@gmail.com>
2026-05-02 19:41:16 -0400

    feact: cambie el fuente de letra

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,74 @@
 import "@workspace/ui/globals.css";
 import { cn } from "@workspace/ui/lib/utils";
 import { QueryProvider } from "@/components/query-provider";
+import localFont from "next/font/local";
+
+const obvia = localFont({
+  src: [
+    {
+      path: "../font/fonnts.com-Obvia_Thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Thin_Italic.otf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Light_Italic.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Book.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Book_Italic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Medium_Italic.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Bold_Italic.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Black.otf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../font/fonnts.com-Obvia_Black_Italic.otf",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+  variable: "--font-obvia",
+});
 
 export default function RootLayout({
   children,
@@ -11,7 +79,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased font-sans")}
+      className={cn(obvia.variable, "font-sans antialiased")}
     >
       <body>
         <QueryProvider>{children}</QueryProvider>
